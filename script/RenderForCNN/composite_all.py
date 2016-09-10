3a7028a67ea314261a9ea9f40307521d7ff67874
Minhyuk Sung <smh0816@gmail.com>
2016-09-10 10:47:02 -0700

    Added 3rdparty scripts.
    Change-Id: I146e157d7d47561b8e4c9547eb95ede7dee98f92

---
+++ script/RenderForCNN/composite_all.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import gflags
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import os, sys, cv2
+
+# 'data_dir' must have 'images' directory including *.png files.
+FLAGS = gflags.FLAGS
+gflags.DEFINE_string('data_dir', '', '')
+gflags.DEFINE_string('output_dir', 'convnet/output', '')
+gflags.DEFINE_string('render_dir', 'convnet/render_all', '')
+gflags.DEFINE_string('composite_dir', 'convnet/composite_all', '')
+
+gflags.DEFINE_string('target_class', 'chair', '')
+gflags.DEFINE_bool('draw_bboxes', True, '')
+gflags.DEFINE_bool('composite_rendered', True, '')
+gflags.DEFINE_bool('half_size_output', True, '')
+
+
+def vis_estimations(image_name):
+    """Draw detected bounding boxes."""
+
+    basename = os.path.splitext(os.path.basename(image_name))[0]
+
+    output_im_file = os.path.join(
+        FLAGS.data_dir, FLAGS.composite_dir, image_name)
+    if os.path.exists(output_im_file):
+        return
+
+    # Load the demo image
+    im_file = os.path.join(FLAGS.data_dir, 'images', image_name)
+    im = cv2.imread(im_file)
+    im_width = np.size(im, 1)
+    im_height = np.size(im, 0)
+
+    # Add alpha channel
+    (B, G, R) = cv2.split(im)
+    im = cv2.merge((B, G, R, B))
+    im[:,:,3] = 255
+
+    # Load detected object bounding boxes
+    bbox_file_list = glob.glob(os.path.join(
+        FLAGS.data_dir, FLAGS.output_dir, basename, '*_bbox.txt'))
+
+    # Composite rendered 3D model rendered images
+    if FLAGS.composite_rendered:
+        for bbox_file in bbox_file_list:
+            bbox_basename = os.path.splitext(os.path.basename(bbox_file))[0]
+            bbox_basename = bbox_basename[:-len('_bbox')]
+            rendered_im_file = os.path.join(FLAGS.data_dir, FLAGS.render_dir,
+                                            basename, bbox_basename + '.png')
+            if not os.path.exists(rendered_im_file):
+                continue
+            print(rendered_im_file)
+
+            dets = np.genfromtxt(bbox_file, delimiter=' ', dtype=float)
+            bbox = dets[:4]
+            bbox_width = bbox[2] - bbox[0]
+            bbox_height = bbox[3] - bbox[1]
+
+            rendered_im = cv2.imread(rendered_im_file, cv2.IMREAD_UNCHANGED)
+            model_im_width = np.size(rendered_im, 1)
+            model_im_height = np.size(rendered_im, 0)
+
+            scale_width = bbox_width / model_im_width
+            scale_height = bbox_height / model_im_height
+            resized_model_im = cv2.resize(
+                rendered_im, None, fx=scale_width, fy=scale_height,
+                interpolation=cv2.INTER_CUBIC)
+
+            sx = max(int(round(bbox[0])), 0)
+            sy = max(int(round(bbox[1])), 0)
+            ex = min(sx + np.size(resized_model_im, 1), np.size(im, 1))
+            ey = min(sy + np.size(resized_model_im, 0), np.size(im, 0))
+            roi = im[sy:ey, sx:ex]
+
+            # Create mask
+            (B, G, R, A) = cv2.split(resized_model_im)
+            mask = A
+            mask_inv = cv2.bitwise_not(mask)
+
+            # Blend two images
+            img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+            img2_fg = cv2.bitwise_and(
+                resized_model_im, resized_model_im, mask=mask)
+            dst = cv2.add(img1_bg, img2_fg)
+            im[sy:ey, sx:ex] = dst
+
+    im = im[:, :, (2, 1, 0)]
+
+    fig = plt.figure(frameon=False)
+    DPI = fig.get_dpi()
+    if FLAGS.half_size_output:
+        DPI = 2 * DPI
+    fig.set_size_inches(im_width/float(DPI), im_height/float(DPI))
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(im, aspect='equal')
+
+    # Draw bounding boxes
+    if FLAGS.draw_bboxes:
+        for bbox_file in bbox_file_list:
+            print(bbox_file)
+            dets = np.genfromtxt(bbox_file, delimiter=' ', dtype=float)
+            bbox = dets[:4]
+            score = dets[-1]
+
+            ax.add_patch(
+                plt.Rectangle((bbox[0], bbox[1]),
+                              bbox[2] - bbox[0],
+                              bbox[3] - bbox[1], fill=False,
+                              edgecolor='red', linewidth=3.5)
+                )
+
+            ax.text(bbox[0], bbox[1] - 2,
+                    '{:s} {:.3f}'.format(FLAGS.target_class, score),
+                    bbox=dict(facecolor='blue', alpha=0.5),
+                    #fontsize=14, color='white')
+                    fontsize=14, color='white')
+
+    # ax.set_title(('{} detections with '
+    #               'p({} | box) >= {:.1f}').format(
+    #     FLAGS.target_class, FLAGS.target_class, thresh), fontsize=14)
+
+    if not os.path.exists(os.path.join(FLAGS.data_dir, FLAGS.composite_dir)):
+        os.makedirs(os.path.join(FLAGS.data_dir, FLAGS.composite_dir))
+    plt.savefig(output_im_file)
+    plt.close(fig)
+
+if __name__ == '__main__':
+    FLAGS(sys.argv)
+
+    im_names = [os.path.basename(x) for x in
+            glob.glob(os.path.join(FLAGS.data_dir, 'images', '*.png'))]
+
+    for im_name in im_names:
+        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+        print 'Demo for data/demo/{}'.format(im_name)
+        vis_estimations(im_name)
+
+    plt.show()
---
+++ script/py-faster-rcnn/detect.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Faster R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+
+import sys
+BASE_DIR = os.path.dirname(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '../../3rdparty/py-faster_rcnn/'))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.dirname(BASE_DIR))
+
+import _init_paths
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from PIL import Image
+from utils.timer import Timer
+import glob
+import gflags
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import caffe, os, sys, cv2
+import argparse
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16', 'VGG16_faster_rcnn_final.caffemodel'),
+        'zf': ('ZF', 'ZF_faster_rcnn_final.caffemodel')}
+
+# 'data_dir' must have 'images' directory including *.png files.
+FLAGS = gflags.FLAGS
+gflags.DEFINE_string('data_dir', '', '')
+gflags.DEFINE_string('output_dir', 'convnet/output', '')
+gflags.DEFINE_string('target_class', 'chair', '')
+gflags.DEFINE_integer('gpu_id', 0, 'GPU device id to use [0]')
+
+gflags.DEFINE_bool('cpu_mode', False, 'Use CPU mode (overrides --gpu)')
+gflags.DEFINE_string('demo_net', 'vgg16', 'Network to use [vgg16]')
+
+'''
+def vis_detections(im, image_name, class_name, dets, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    #if len(inds) == 0:
+    #    return
+
+    im = im[:, :, (2, 1, 0)]
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(im, aspect='equal')
+    for i in inds:
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1], fill=False,
+                          edgecolor='red', linewidth=3.5)
+            )
+        ax.text(bbox[0], bbox[1] - 2,
+                '{:s} {:.3f}'.format(class_name, score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
+
+    ax.set_title(('{} detections with '
+                  'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  thresh),
+                  fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+
+    if not os.path.exists(os.path.join(FLAGS.data_dir, FLAGS.output_dir)):
+        os.makedirs(os.path.join(FLAGS.data_dir, FLAGS.output_dir))
+
+    detection_im_file = os.path.join(FLAGS.data_dir, FLAGS.output_dir, image_name)
+    plt.savefig(detection_im_file, bbox_inches='tight')
+    plt.close(fig)
+'''
+
+def write_cropped_images(imdata, image_name, class_name, dets, thresh=0.5):
+    """Write cropped images."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    print ('{} {}(s) are detected.'.format(len(inds), FLAGS.target_class))
+    if len(inds) == 0:
+        return
+
+    basename = os.path.splitext(image_name)[0]
+    print(os.path.join(FLAGS.data_dir, FLAGS.output_dir, basename))
+    if not os.path.exists(os.path.join(FLAGS.data_dir, FLAGS.output_dir, basename)):
+        os.makedirs(os.path.join(FLAGS.data_dir, FLAGS.output_dir, basename))
+
+    imdata = imdata[:, :, (2, 1, 0)]
+
+    for i in inds:
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+        im = Image.fromarray(imdata, 'RGB')
+        cropped_im = im.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
+        cropped_im_file = os.path.join(FLAGS.data_dir, FLAGS.output_dir, basename,
+                class_name + '_' + str(i))
+        print(cropped_im_file)
+        cropped_im.save(cropped_im_file + '.png')
+        np.savetxt(cropped_im_file + '_bbox.txt',
+                dets[i, :].reshape(1, 5), delimiter=' ', fmt='%f')
+
+def demo(net, image_name):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load the demo image
+    im_file = os.path.join(FLAGS.data_dir, 'images', image_name)
+    im = cv2.imread(im_file)
+
+    # Detect all object classes and regress object bounds
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(net, im)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+    print ('Target class: {}'.format(FLAGS.target_class))
+
+    # Visualize detections for each class
+    CONF_THRESH = 0.8
+    NMS_THRESH = 0.3
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+        #
+        if cls != FLAGS.target_class:
+            continue
+        #
+        cls_ind += 1 # because we skipped background
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+        write_cropped_images(im, image_name, cls, dets, thresh=CONF_THRESH)
+        #vis_detections(im, image_name, cls, dets, thresh=CONF_THRESH)
+
+if __name__ == '__main__':
+    FLAGS(sys.argv)
+
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+
+    prototxt = os.path.join(cfg.MODELS_DIR, NETS[FLAGS.demo_net][0],
+                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
+                              NETS[FLAGS.demo_net][1])
+
+    if not os.path.isfile(caffemodel):
+        raise IOError(('{:s} not found.\nDid you run ./data/script/'
+                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+
+    if FLAGS.cpu_mode:
+        caffe.set_mode_cpu()
+    else:
+        caffe.set_mode_gpu()
+        caffe.set_device(FLAGS.gpu_id)
+        cfg.GPU_ID = FLAGS.gpu_id
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+    print '\n\nLoaded network {:s}'.format(caffemodel)
+
+    # Warmup on a dummy image
+    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+    for i in xrange(2):
+        _, _= im_detect(net, im)
+
+    #im_names = ['MVI_0018_001.png']
+    im_names = [os.path.basename(x) for x in
+            glob.glob(os.path.join(FLAGS.data_dir, 'images', '*.png'))]
+
+    for im_name in im_names:
+        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+        print 'Demo for {}'.format(im_name)
+        demo(net, im_name)
+
+    plt.show()
