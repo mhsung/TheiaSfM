339dd87ed4b3b82f96e1e88a3b99df27f246ac6e
Minhyuk Sung <msung@minhyuks-mbp.corp.adobe.com>
2016-09-15 17:59:08 -0700

    Changed script shebangs.
    Change-Id: Iff62910c3cb4038bd2842a78592ed0a6a078e5a8

--- script/RenderForCNN/example.sh
+++ script/RenderForCNN/example.sh
@@ -1,28 +1,32 @@
 #!/usr/bin/env bash
 
+# Change variables.
+DATA_DIR=/Users/msung/Developer/data/MVI_0219
+TARGET_CLASS=car
+
 # Detect objects.
-../py-faster-rcnn/detect.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
-    --target_class=car
+../py-faster-rcnn/detect.py
+    --data_dir=${DATA_DIR} \
+    --target_class=${TARGET_CLASS}
 
 # Estimate best orientation.
 ./estimate.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
-    --target_class=car
+    --data_dir=${DATA_DIR} \
+    --target_class=${TARGET_CLASS}
 
 # Estimate orientation scores.
 ./estimate_pred.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
-    --target_class=car
+    --data_dir=${DATA_DIR} \
+    --target_class=${TARGET_CLASS}
 
 # Find the largest bounding box.
 ./find_largest.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
-    -target_class=car
+    --data_dir=${DATA_DIR} \
+    -target_class=${TARGET_CLASS}
 
 # Fit orientation in the time sequence.
 ../run_plot.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
+    --data_dir=${DATA_DIR} \
 	--param_data_names=''\
 	--param_data_dirs='' \
 	--convnet_dir=convnet/largest/preds \
@@ -31,32 +35,32 @@
 
 # Interpolate orientations.
 ../run_interpolate_camera_params.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
+    --data_dir=${DATA_DIR} \
     --input_param_data_dir=convnet/largest/fitted_views \
     --output_param_data_dir=convnet/largest/interp_fitted_views
 
 # Interpolate bounding boxes.
 ../run_interpolate_bboxes.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
+    --data_dir=${DATA_DIR} \
 	--input_bbox_data_dir=convnet/largest/bboxes \
 	--output_bbox_data_dir=convnet/largest/interp_bboxes
 
 # Render 3D model.
 ./render_object.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
+    --data_dir=${DATA_DIR} \
     --view_dir=convnet/largest/interp_fitted_views \
     --render_dir=convnet/largest/render_interp_fitted_views \
-    --target_class=car
+    --target_class=${TARGET_CLASS}
 
 # Composite images.
 ./composite_object.py \
-    --data_dir=/Users/msung/Developer/data/MVI_0219 \
+    --data_dir=${DATA_DIR} \
     --render_dir=convnet/largest/render_interp_fitted_views \
     --bbox_dir=convnet/largest/interp_bboxes \
     --composite_dir=convnet/largest/composite_interp_fitted \
-    --target_class=car
+    --target_class=${TARGET_CLASS}
 
 # Create video.
-ffmpeg -i /Users/msung/Developer/data/MVI_0219/convnet/largest/composite_interp_fitted/MVI_0219_%04d.png \
+ffmpeg -i ${DATA_DIR}/convnet/largest/composite_interp_fitted/MVI_0219_%04d.png \
     -pix_fmt yuv420p -r 24 -b:v 8000k -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
-    /Users/msung/Developer/data/MVI_0219/convnet/largest/composite_interp_fitted.mp4
+    ${DATA_DIR}/convnet/largest/composite_interp_fitted.mp4
--- script/options.py
+++ script/options.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Author: Minhyuk Sung (mhsung@cs.stanford.edu)
 
 import gflags
