b6888ad7fabf4912f2575a3d5ee1aa08ef9cc8b5
Minhyuk Sung <smh0816@gmail.com>
2016-10-07 14:16:55 -0700

    Minor update.
    Change-Id: I305f1c0d1e31ff7cef064f5fefc334bba3b0718d

--- script/match.py
+++ script/match.py
@@ -26,14 +26,14 @@ def run(FLAGS, PATHS):
         cmd += '--match_only_consecutive_pairs=true' + ' \\\n'
         cmd += '--consecutive_pair_frame_range=' + \
                str(FLAGS.seq_range) + ' \\\n'
-    if FLAGS.less_num_inliers:
-        cmd += '--min_num_inliers_for_valid_match=10' + ' \\\n'
-    if FLAGS.less_sampson_error:
-        cmd += '--max_sampson_error_for_verified_match=10.0' + ' \\\n'
-    if FLAGS.no_two_view_bundle:
-        cmd += '--bundle_adjust_two_view_geometry=false' + ' \\\n'
-    if FLAGS.no_only_symmetric:
-        cmd += '--keep_only_symmetric_matches=false' + ' \\\n'
+    # if FLAGS.less_num_inliers:
+    #     cmd += '--min_num_inliers_for_valid_match=10' + ' \\\n'
+    # if FLAGS.less_sampson_error:
+    #     cmd += '--max_sampson_error_for_verified_match=10.0' + ' \\\n'
+    # if FLAGS.no_two_view_bundle:
+    #     cmd += '--bundle_adjust_two_view_geometry=false' + ' \\\n'
+    # if FLAGS.no_only_symmetric:
+    #     cmd += '--keep_only_symmetric_matches=false' + ' \\\n'
 
     # FIXME:
     # Should we use initial orientation in matching?
